41d15c3af5153cfcfdbf4c25728fcce38334e7f7
Marc Frances <marc.frances.t@gmail.com>
2023-07-14 18:29:13 +0200

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const { isNumber } = require("util");
 
+const sumAll = function(x, y) {
+    if (x < 0|| y < 0 || !Number.isInteger(x) || !Number.isInteger(y) ) {
+        return "ERROR";
+    }
+
+    let min = x;
+    let max = y;
+    if (min > max) {
+        min = y;
+        max = x;
+    }
+
+    let res = 0;
+    for (let i = min; i <= max; i++) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
