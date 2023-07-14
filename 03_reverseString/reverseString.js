edd2e82101226a46896a9b656245bf5e95c36025
Marc Frances <marc.frances.t@gmail.com>
2023-07-14 17:20:26 +0200

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const letters = str.split('');
+    let res = [];
+    for (let i = letters.length - 1; i >= 0; i--) {
+        res.push(letters[i]);
+    }
+    return res.join('');
+    //return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
