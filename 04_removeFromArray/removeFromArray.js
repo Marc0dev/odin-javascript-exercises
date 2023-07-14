286cbffb5686a8e3811c71262fe94d8466fa36f4
Marc Frances <marc.frances.t@gmail.com>
2023-07-14 18:12:26 +0200

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (const e of elements) {
+        let pos = array.indexOf(e);
+        if (pos !== -1) {
+            array.splice(pos, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
