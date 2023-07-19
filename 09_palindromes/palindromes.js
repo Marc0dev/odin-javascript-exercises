0bbd6d186f498857416eafa132c1e35ba527c754
Marc Frances <marc.frances.t@gmail.com>
2023-07-19 23:22:45 +0200

    Complete JS exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const sanitizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return sanitizedStr === sanitizedStr.split('').reverse().join('');;
 };
 
 // Do not edit below this line
