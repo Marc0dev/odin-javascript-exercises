c3f88555a4384f3b37c36c62673efde9604a0b83
Marc Frances <marc.frances.t@gmail.com>
2023-07-19 23:23:05 +0200

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num <= 0) return 'OOPS';
+    num = Number(num);
 
+    let current = 1;
+    let pre = 0;
+    for (let i = 1; i < num; i++) {
+        let temp = current;
+        current += pre;
+        pre = temp;
+    }
+    return current;
 };
 
 // Do not edit below this line
