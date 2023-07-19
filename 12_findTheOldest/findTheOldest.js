7bd54ef52bd2680a7067946fac88b69eac8d0712
Marc Frances <marc.frances.t@gmail.com>
2023-07-19 23:23:56 +0200

    Complete JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const actualDate = (new Date()).getFullYear();
+    return array.reduce((oldest, person) => {
+        if (!person.yearOfDeath) person.yearOfDeath = actualDate;
+        const personYears = person.yearOfDeath - person.yearOfBirth;
+        const oldestYears = oldest.yearOfDeath - oldest.yearOfBirth;
 
+        if (personYears > oldestYears) return person;
+        return oldest;
+        
+    }, {yearOfBirth: actualDate, yearOfDeath: actualDate});
 };
 
 // Do not edit below this line
