979289306079b1a95e18986aa1e7f83c51eb2a8b
Marc Frances <marc.frances.t@gmail.com>
2023-07-19 23:22:23 +0200

    Complete JS exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acum, num) => acum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...values) {
+  return values.reduce((acum, num) => acum * num, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  return (x === 0) ? 1 : x * factorial(x - 1);
 };
 
 // Do not edit below this line
